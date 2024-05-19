369a76a8d33fe1f09d035cf20b5331c4e5da1423
Ckala62rus <agr.akyla@mail.ru>
2024-05-19 21:36:33 +0300

    (fix) add default logic for user entity.

--- users/admin.py
+++ users/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from django.contrib.auth import get_user_model
 from django.contrib.auth.admin import UserAdmin
+from django.utils.translation import gettext_lazy as _
 
 from users.models.users import User
 
@@ -8,4 +9,25 @@ from users.models.users import User
 # Register your models here.
 @admin.register(User)
 class UserAdmin(UserAdmin):
-    pass
+    readonly_fields = ("id",)
+    list_display = ("id", "username", "email", "first_name", "last_name", "is_staff")
+    fieldsets = (
+        (None, {
+            "fields": ("id",)
+        }),
+        (None, {"fields": ("username", "password")}),
+        (_("Personal info"), {"fields": ("first_name", "last_name", "email")}),
+        (
+            _("Permissions"),
+            {
+                "fields": (
+                    "is_active",
+                    "is_staff",
+                    "is_superuser",
+                    "groups",
+                    "user_permissions",
+                ),
+            },
+        ),
+        (_("Important dates"), {"fields": ("last_login", "date_joined")}),
+    )
